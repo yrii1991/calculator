b9f211b98b7ede2abd02ed12f918ea1ab73140f5
Yrik <tishenkoyra1991@gmail.com>
2020-10-12 23:50:37 +0300

    handler dot

--- js/calc.js
+++ js/calc.js
@@ -1,14 +1,17 @@
 function calc () {
     let display = document.querySelector('.calc_display');
     let keyboard = document.querySelector('.calc_keyboard_number');
+    let x;
 
     keyboard.addEventListener('click', function (event) {
         let displayText =  display.innerText;
         if (event.target.innerText === 'C') {
             display.innerHTML = '';
         } else {
-            // console.log(event.target.innerText);
-            display.innerHTML = displayText + event.target.innerText;
+            if (event.target.innerText ==="." && displayText.includes('.')) {
+            } else {
+                display.innerHTML = displayText + event.target.innerText;
+            }
         }
     })
 
